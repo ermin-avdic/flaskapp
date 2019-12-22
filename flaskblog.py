6ebc56989f20b5fd554ef66b6a449c193742715b
Ermin <usb007agent@gmail.com>
2019-12-22 22:50:19 +0100

    Restructuring project files

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-from datetime import datetime
+from models import User, Post
 
 app = Flask(__name__)
 
@@ -9,42 +9,6 @@ app.config["SECRET_KEY"] = "82f1d96a3652b640747d5353f0f97eb6 "
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 
 db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
-    password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship("Post", backref="author", lazy=True)
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}'. '{self.image_file}')"
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
-
-posts = [
-    {
-        'author': 'Ermin Avdic',
-        'title': 'Blog post one',
-        'content': 'First blog post',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Ermin Avdic',
-        'title': 'Blog post two',
-        'content': 'Second blog post',
-        'date_posted': 'April 20, 2018'
-    }
-]
 
 @app.route("/")
 @app.route("/home")
